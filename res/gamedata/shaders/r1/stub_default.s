296a38f1f0eae769d02c51b1be19a50b3eacdf36
Xottab-DUTY <xottab-duty@yandex.ru>
2020-06-08 17:07:47 +0500

    Fixed some shaders

--- res/gamedata/shaders/r1/stub_default.s
+++ res/gamedata/shaders/r1/stub_default.s
@@ -2,7 +2,5 @@ function normal		(shader, t_base, t_second, t_detail)
 	shader:begin	("stub_default","stub_default")
 
 --	Decouple sampler and texture
---	shader:sampler	("s_base")	: texture(t_base)	: clamp() : f_linear ()
---	TODO: DX10: move stub_default to smp_rtlinear
-	shader:sampler	("smp_base")		:texture	(t_base)
+	shader:sampler	("s_base")	: texture(t_base)	: clamp() : f_linear ()
 end
