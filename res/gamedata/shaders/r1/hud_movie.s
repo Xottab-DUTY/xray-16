75f3a8705576294b588cf79585133e8e00620b52
Xottab-DUTY <xottab-duty@yandex.ru>
2020-06-08 17:07:47 +0500

    Fixed some shaders

--- res/gamedata/shaders/r1/hud_movie.s
+++ res/gamedata/shaders/r1/hud_movie.s
@@ -1,5 +1,5 @@
 function normal		(shader, t_base, t_second, t_detail)
-	shader:begin	("stub_notransform_t","yuv2rgb")
+	shader:begin	("yuv2rgb","yuv2rgb")
 			: fog		(false)
 			: zb 		(false,false)
 			: blend		(true,blend.srcalpha,blend.invsrcalpha)
